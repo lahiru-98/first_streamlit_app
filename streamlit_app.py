7d978e34b8f0bdaea9fbe19fe1febd6d50e374ba
Lahiru Nuwan <59309257+lahiru-98@users.noreply.github.com>
2022-05-17 09:22:28 +0530

    Update streamlit_app.py
    Enabled : -  Display only the selected Fruit types (filter)

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Allow user to select set of fruits that they want to include
-streamlit.multiselect("Pick Some Fruits : ", list(my_fruit_list.index) , ['Avocado', 'Apple'])
+fruits_selected = streamlit.multiselect("Pick Some Fruits : ", list(my_fruit_list.index) , ['Avocado', 'Apple'])
+
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(my_fruit_list) 
