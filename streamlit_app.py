73a669a4ab5ccea13ea249206c4e22370fec4c44
Lahiru Nuwan <59309257+lahiru-98@users.noreply.github.com>
2022-05-17 09:09:58 +0530

    Update streamlit_app.py
    read a csv file and displayed it as a datsframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text(" 🐔 Hard Boild Free-Ramge-Egg")
 streamlit.text(" 🥑🍞 Avacado toast")
 
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+
+#Reading the csv file from a S3 bucket
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list) 
