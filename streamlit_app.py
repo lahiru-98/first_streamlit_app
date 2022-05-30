6a5989c8452855ba43cdb6d4519e12dcd4b684f5
Lahiru Nuwan <59309257+lahiru-98@users.noreply.github.com>
2022-05-30 14:30:22 +0530

    error - fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,5 +27,5 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("FruityVice Fruit Advice")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-fruityvice_normalized = pandas.json_normalize(ruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
